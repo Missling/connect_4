e734e5a7fda904f809f04127e02d8bbd483d4955
Eugene Shuster & Missling <pair+eshuster.missling@devbootcamp.com>
2015-04-16 18:01:48 -0700

    completed the board

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,58 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var double_click = function(current) {
+    var column = $(current).attr('class');
+    for (var i=6; i>0; i--){
+      if ($("#" + i + " ."+column).hasClass('filled_red') || $("#" + i + " ."+column).hasClass('filled_blue')){
+      } else {
+        $("#" + i + " ."+column).addClass('filled_red')
+        break;
+      } //if
+    } //for
+  } //single_click
+
+  var single_click = function(current) {
+    var column = $(current).attr('class');
+    for (var i=6; i>0; i--){
+      if ($("#" + i + " ."+column).hasClass('filled_blue') || $("#" + i + " ."+column).hasClass('filled_red')){
+      } else {
+        $("#" + i + " ."+column).addClass('filled_blue')
+        // var cell = $("#" + i + " ."+column))
+        // winner(cell)
+        break;
+      } //if
+    } //for
+  } //single_click
+
+  // var winner = function(cell) {
+  //   var vertical_check = function(cell) {
+  //     var horizontalCheck = function(){
+  //       for (var i=0; i<5; i++){
+  //         var row = $("#" + i + " ."+column)
+  //         if // row of 4
+  //       }
+  //     }
+  //   }
+
+  // } //winner
+
+  var DELAY = 300, clicks = 0, timer = null;
+  $("td").on("click", function(e){
+    var current = this
+    clicks++;  //count clicks
+      if(clicks === 1) {
+        timer = setTimeout(function() {
+          single_click(current);
+          clicks = 0;             //after action performed, reset counter
+          }, DELAY);
+      }
+      else {
+        clearTimeout(timer);    //prevent single-click action
+        double_click(current);
+        clicks = 0;
+      }
+  })
+    .on("dblclick", function(e){
+        e.preventDefault();  //cancel system double-click event
+      });
 });
